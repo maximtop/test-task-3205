129b2b6b5ca192756e4d7dce8c086283d67bdd8a
Maxim Topchu <maximtop@gmail.com>
2019-03-04 11:42:39 +0300

    remove console.log

--- src/js/background.js
+++ src/js/background.js
@@ -2,13 +2,12 @@ import '../img/icon-128.png'
 import '../img/icon-34.png'
 
 const downloadImage = (src) => {
-  console.log(src);
   chrome.downloads.download({
     url: src,
   })
 };
 
-chrome.runtime.onMessage.addListener((message, sender) => {
+chrome.runtime.onMessage.addListener((message) => {
   const { type } = message;
   switch (type) {
     case 'download':
